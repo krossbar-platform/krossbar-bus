39905fe4c38cc32b97d7228ebc5aeb1e6e554994
Alexander Smoktal <cosm.ua@gmail.com>
2022-07-09 16:50:58 +0300

    Fix build.

--- caro-macros/src/lib.rs
+++ caro-macros/src/lib.rs
@@ -1,34 +1,34 @@
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{self, parse::Parser, Field, Fields, ItemStruct};
+use syn::{self, ItemStruct};
 
 #[proc_macro_attribute]
 pub fn service(metadata: TokenStream, input: TokenStream) -> TokenStream {
     let metadata: syn::LitStr = syn::parse(metadata).unwrap();
 
-    let mut service_struct: ItemStruct = syn::parse(input).unwrap();
-    match &mut service_struct.fields {
-        Fields::Named(fields) => fields
-            .named
-            .push(Field::parse_named.parse2(quote! { _name: String }).unwrap()),
-        _f => {
-            return quote!(compile_error!(format!(
-                "Expected structure to have names fields, got {:?}",
-                _f
-            ),))
-            .into();
-        }
-    }
+    let service_struct: ItemStruct = syn::parse(input).unwrap();
+    // match &mut service_struct.fields {
+    //     Fields::Named(fields) => fields
+    //         .named
+    //         .push(Field::parse_named.parse2(quote! { _name: String }).unwrap()),
+    //     _f => {
+    //         return quote!(compile_error!(format!(
+    //             "Expected structure to have names fields, got {:?}",
+    //             _f
+    //         ),))
+    //         .into();
+    //     }
+    // }
 
     let struct_ident = &service_struct.ident;
     let implementation_stream = quote! {
         impl caro_bus_lib::Service for #struct_ident {
             fn init(&mut self) {
-                self._name = #metadata.into()
+                //self._name = #metadata.into()
             }
 
             fn print(&self) {
-                println!("Service name: {}", self._name)
+                println!("Service name: {}", #metadata)
             }
         }
     };
