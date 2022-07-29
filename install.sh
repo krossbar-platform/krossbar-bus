6ee65bd0995e502506bf847bc9a4c0845d038bc1
Alexander Smoktal <cosm.ua@gmail.com>
2022-07-29 17:56:07 +0300

    Add connect and monitor install scripts.

--- install.sh
+++ install.sh
@@ -8,3 +8,13 @@ echo -e "\e[32mInstalling bus hub\e[0m"
 pushd ${self_dir}/caro-bus-hub/ > /dev/null
 cargo make --makefile Make.toml install
 popd > /dev/null
+
+echo -e "\e[32mInstalling bus connect\e[0m"
+pushd ${self_dir}/caro-bus-connect/ > /dev/null
+cargo make --makefile Make.toml install
+popd > /dev/null
+
+echo -e "\e[32mInstalling bus monitor\e[0m"
+pushd ${self_dir}/caro-bus-monitor/ > /dev/null
+cargo make --makefile Make.toml install
+popd > /dev/null
